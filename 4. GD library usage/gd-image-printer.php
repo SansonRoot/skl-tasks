c93889eb084956543df28a39ddbb4b05a925bf51
aaron san-yuo <asanyuo123@gmail.com>
2021-12-17 20:01:25 +0000

    chore: minor updates to gd checker display

--- 4. GD library usage/gd-image-printer.php
+++ 4. GD library usage/gd-image-printer.php
@@ -3,7 +3,40 @@
 header('Content-Type: image/png');
 
 //fetch parameters passed to url
-$width = $_GET['width'];
-$height = $_GET['height'];
-$length = $_GET['length'];
+$width = isset($_GET['width']) ? $_GET['width'] : 500;
+$height = isset($_GET['height']) ? $_GET['height'] : 500;
+$length = isset($_GET['length']) ? $_GET['length'] : 50;
 
+$column_count = $width/$length;
+$row_count = $height/$length;
+
+$img_blank = imagecreatetruecolor($width,$height);
+
+//white_bg
+$white_bgc = imagecolorallocate($img_blank,255,255,255);
+
+//black_bg
+$black_bgc = imagecolorallocate($img_blank,0,0,0);
+
+
+//start with black
+$black = false;
+
+for ($i = 0;$i<$row_count;$i++){
+
+    for ($j = 0;$j<$column_count;$j++){
+
+        //display black
+        if (!$black){
+            imagefilledrectangle($img_blank,$j*$length,$i*$length,($j+1)*$length,($i+1)*$length, $black_bgc);
+        }else{
+            //display white
+            imagefilledrectangle($img_blank,$j*$length,$i*$length,($j+1)*$length,($i+1)*$length, $white_bgc);
+        }
+        $black = !$black;
+    }
+
+}
+
+imagepng($img_blank);
+imagedestroy($img_blank);
