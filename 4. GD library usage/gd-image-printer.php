9f7e7f9922e0bdc322a3c41d12bb18e975c47a22
aaron san-yuo <asanyuo123@gmail.com>
2021-12-17 19:03:38 +0000

    chore: minor updates to gd task

--- 4. GD library usage/gd-image-printer.php
+++ 4. GD library usage/gd-image-printer.php
@@ -1,48 +1,9 @@
 <?php
-//
-//header('Content-Type: image/png');
-//
-//$width = $_GET['width'];
-//$height = $_GET['height'];
-//$length = $_GET['length'];
-//
-//$im = imagecreatefrompng("boost-logo.png");
-//
-//$white = imagecolorallocate($im, 255, 255, 255);
-//$black = imagecolorallocate($im, 0, 0, 0);
-////
-////$res='';
-////
-//////$px = (imagesx($im) - 7.5 * strlen('new string length')) / 2;
-////$string = imagestring($im, 1, 1, 3, $res, $black);
-//////imagepng($im);
-////imagecreatefromstring($string);
-////imagedestroy($im);
-//
-//$dw = $width * 2;
-//$dh = $height * 2;
-//
-//// Now copy the entire switch statement from above; the loops can be reduced
-//// to 4-5 `imagecopy` statements each instead of loops if you want.
-//
-//// Create the actual output image:
-//$outputimg = imagecreatetruecolor(500, 500);
-////$destimg = imagecreatetruecolor($dw, $dh);
-////
-////// Set tile and use the fill functions...
-//imagesettile($outputimg,$im);
-//imagefilledrectangle($outputimg, 0, 0,20,20, $white);
-//
-//header('image/png');
-//imagepng($outputimg);
 
-header('content-type: image/png');
+header('Content-Type: image/png');
 
-$board = imagecreatetruecolor(500, 500);
+//fetch parameters passed to url
+$width = $_GET['width'];
+$height = $_GET['height'];
+$length = $_GET['length'];
 
-$image = imagecreatetruecolor(20, 20);
-$image = imagecreatefrompng('boost-logo.png');  //Path to the image file
-
-imagecopymerge($image, $board, 5, 5, 0, 0, 500, 500, 100);
-imagejpeg($image);
-imagedestroy($image);
